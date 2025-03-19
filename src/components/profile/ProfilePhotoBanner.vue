7ec72a19f8d668adc920bd9ae7302ca19b62cb94
shogunIncognito <saidsamuel2006@hotmail.com>
2025-03-19 08:51:55 -0500

    Actualizar el manejo de carga de imágenes en el componente ProfilePhotoBanner para soportar múltiples estados de carga

--- src/components/profile/ProfilePhotoBanner.vue
+++ src/components/profile/ProfilePhotoBanner.vue
@@ -7,13 +7,16 @@ import Spinner from '../Spinner.vue';
 
 const { user } = defineProps(['user']);
 const emit = defineEmits(['update-image']);
-const isUploading = ref(false);
+const isUploading = ref({
+    photoPerfil: false,
+    backgroundPhoto: false,
+});
 
 const handleImageToUpload = async (file, type) => {
     if (!file) return;
 
     try {
-        isUploading.value = true;
+        isUploading.value = { ...isUploading.value, [type]: true };
         const formData = new FormData();
         formData.append(type, file);
 
@@ -32,12 +35,12 @@ const handleImageToUpload = async (file, type) => {
     } catch (error) {
         console.error('Error al actualizar la imagen:', error);
     } finally {
-        isUploading.value = false;
+        isUploading.value = { ...isUploading.value, [type]: false };
     }
 };
 
 const handleFileChange = (event, type) => {
-    if (isUploading.value) return;
+    if (isUploading.value.photoPerfil || isUploading.value.backgroundPhoto) return;
     const file = event.target.files[0];
     if (file) {
         handleImageToUpload(file, type);
@@ -52,8 +55,8 @@ const handleFileChange = (event, type) => {
             <img :src="user.photoBackground?.url || 'https://www.cristianroldan.art/wp-content/uploads/2020/10/escaparate-pintado-a-mano-estudio-de-tatuaje.jpg'"
                 alt="banner del tatuador" class="w-full h-52 object-cover rounded" />
             <label
-                :class="`absolute inset-0 flex items-center justify-center bg-black/40 opacity-0 group-hover:opacity-100 transition-opacity cursor-pointer ${isUploading ? 'cursor-not-allowed opacity-100' : ''}`">
-                <Spinner v-if="isUploading" class="w-8 h-8 text-white" />
+                :class="`absolute inset-0 flex items-center justify-center bg-black/40 opacity-0 group-hover:opacity-100 transition-opacity cursor-pointer ${isUploading.backgroundPhoto ? 'cursor-not-allowed opacity-100' : ''}`">
+                <Spinner v-if="isUploading.backgroundPhoto" class="w-8 h-8 text-white" />
                 <TbPencilDiscount v-else class="w-8 h-8 text-white" />
                 <input type="file" accept="image/*" class="hidden"
                     @change="(e) => handleFileChange(e, 'backgroundPhoto')" />
@@ -64,8 +67,8 @@ const handleFileChange = (event, type) => {
                 <img :src="user.photoPerfil?.url || user.profileImageGoogle || 'https://th.bing.com/th?id=OIF.xfLzb0EOnt2D%2bhjO2WcEpw&rs=1&pid=ImgDetMain'"
                     alt="Foto del tatuador" class="w-40 h-40 object-cover rounded-full border-4 border-white" />
                 <label
-                    :class="`absolute inset-0 flex items-center justify-center bg-black/40 opacity-0 group-hover:opacity-100 transition-opacity cursor-pointer rounded-full ${isUploading ? 'cursor-not-allowed opacity-100' : ''}`">
-                    <Spinner v-if="isUploading" class="w-8 h-8 text-white" />
+                    :class="`absolute inset-0 flex items-center justify-center bg-black/40 opacity-0 group-hover:opacity-100 transition-opacity cursor-pointer rounded-full ${isUploading.photoPerfil ? 'cursor-not-allowed opacity-100' : ''}`">
+                    <Spinner v-if="isUploading.photoPerfil" class="w-8 h-8 text-white" />
                     <TbPencilDiscount v-else class="w-8 h-8 text-white" />
                     <input type="file" accept="image/*" class="hidden"
                         @change="(e) => handleFileChange(e, 'profilePhoto')" />
